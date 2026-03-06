95fe78c742c63ae183a661a76e7dda3abfa48d03
limeng <limeng@bogon>
2026-03-06 14:37:59 +0800

    Switch next.config.ts to next.config.mjs to fix type errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
-  // @ts-ignore
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
